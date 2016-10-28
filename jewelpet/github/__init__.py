1f82b97ca0f128bc2b685c12097fd1d6933e8f80
Ryo Miyake <ryo.studiom@gmail.com>
2016-10-28 15:30:04 +0900

    rough impl github and travis

--- jewelpet/github/__init__.py
+++ jewelpet/github/__init__.py
@@ -1,5 +1,7 @@
 import hmac
 
+import github
+
 from jewelpet.conf import settings
 
 
@@ -17,3 +19,71 @@ def is_valid_signature(body, api_signature):
         bytes(settings['github']['hook_secret'], 'utf-8'),
         msg=body, digestmod='sha1').hexdigest()
     return 'sha1=%s' % generated == api_signature
+
+
+class Session(object):
+    def __init__(self):
+        """
+        """
+        self._g = github.Github(settings['github']['token'])
+        self.user = self._g.get_user()
+
+    def get_user(self, name):
+        """
+        Args:
+            <string>
+        Returns:
+            <NamedUser>
+        """
+        return self._g.get_user(name)
+
+    def get_organization(self, name):
+        """
+        Args:
+            <string>
+        Returns:
+            <Organization>
+        """
+        return self._g.get_organization(name)
+
+    def find_repo(self, req):
+        """
+        Args:
+            <dict> request parameter of repository from GitHub API
+                    {
+                        "name": "repository name",
+                        "owner": {
+                            "login": "owner login",
+                            "type": "User or Organization"
+                        }
+                    }
+        Returns:
+            <Repository>
+        """
+        owner_type = req['owner']['type']
+        if owner_type not in ('User', 'Organization'):
+            raise Exception('Unknown owner type "%s"' % owner_type)
+        owner = getattr(self._g, 'get_%s' % owner_type.lower())(req['owner']['login'])
+        return owner.get_repo(req['name'])
+
+
+def build_auto(repo, pr_number, mode):
+    assert not is_auto_branch_exists(repo)
+    head = repo.get_commit('HEAD')
+    repo.create_git_ref('refs/heads/auto', head.sha)  # create auto branch
+
+    pr = repo.get_pull(pr_number)
+    repo.merge('auto', pr.head.sha, '%s: auto merge branch "%s"' % (mode, pr.head.ref))
+
+
+def is_auto_branch_exists(repo):
+    """
+    Returns:
+        <bool>
+    """
+    try:
+        if repo.get_branch('auto'):
+            return True
+    except github.GithubException as e:
+        assert e.status == 404
+        return False
--- web.py
+++ web.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import json
+
 from flask import Flask, request
 
 import jewelpet.slack.commands
+from jewelpet import github, slack
 from jewelpet.conf import settings
 
 app = Flask(__name__)
@@ -14,12 +19,70 @@ def slack_app():
         return '', 403
 
     (bot_name, command, *args) = args.get('text').split(' ')
-    if bot_name != settings['bot_name']:
+    if bot_name != settings['slack']['bot_name']:
         return '{"text": "who?"}'
     method = getattr(jewelpet.slack.commands, command, None)
     if not method:
         return '{"text":"nothing"}'
     return '{"text":"%s"}' % method(*args)
+
+
+@app.route('/github', methods=['POST'])
+def github_app():
+    req = request.json
+    if not github.is_valid_signature(
+            request.data,
+            request.headers.get('X-Hub-Signature')):
+        return 'Invalid Signature'
+    try:
+        if req['action'] != 'created':
+            return 'PASS'
+        (trigger, command, *args) = req['comment']['body'].split(' ')
+
+        if trigger in settings['github']['reviewers'] and command == 'r?':
+            s = github.Session()
+            repo = s.find_repo(req['repository'])
+            issue = repo.get_issue(int(req['issue']['number']))
+            issue.edit(assignee=s.get_user(trigger[1:]))
+            return 'ASSIGNED'
+
+        if trigger != settings['github']['trigger']:
+            return 'PASS'
+        if command == 'try':
+            s = github.Session()
+            repo = s.find_repo(req['repository'])
+            if github.is_auto_branch_exists(repo):
+                slack.post('"auto" branch is already exists')
+                return 'AUTO ALREADY EXISTS'
+
+            pr_number = int(req['issue']['number'])
+            github.build_auto(repo, pr_number, 'try')
+            slack.post('created "auto" branch')
+            return 'AUTO'
+    except ValueError:
+        return 'ValueError'
+    except KeyError:
+        return 'KeyError'
+
+
+@app.route('/travis', methods=['POST'])
+def travis_app():
+    req = json.loads(request.form.get('payload'))
+    from pprint import pprint
+    pprint(req)
+    if req.get('branch', '') != 'auto':
+        return 'PASS'
+
+    # state = (passed|failed)
+    state = req.get('state', '')
+    if state == 'passed':
+        slack.post('"auto"のビルドが成功したようだな')
+        return 'OK'
+    elif state == 'failed':
+        slack.post('"auto"のビルドは失敗したようだな')
+        return 'NG'
+    else:
+        slack.post('"auto"のビルドがよく分からん')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
