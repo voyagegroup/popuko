6e2a5f48edbc0258eb632b7c790ac9d577fbd749
Ryo Miyake <ryo.studiom@gmail.com>
2016-10-28 13:48:08 +0900

    jewelpet.github first impl

---
+++ jewelpet/github/__init__.py
@@ -0,0 +1,19 @@
+import hmac
+
+from jewelpet.conf import settings
+
+
+def is_valid_signature(body, api_signature):
+    """
+    Validate GitHub API signature
+
+    Args:
+        <string> request body
+        <string> API request header 'X-Hub-Signature'
+    Returns:
+        <bool> request is valid or not
+    """
+    generated = hmac.new(
+        bytes(settings['github']['hook_secret'], 'utf-8'),
+        msg=body, digestmod='sha1').hexdigest()
+    return 'sha1=%s' % generated == api_signature
