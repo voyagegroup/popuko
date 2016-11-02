4b0f6a641d62aaae469b67e6568e3d2310258707
Ryo Miyake <ryo.studiom@gmail.com>
2016-11-02 13:17:19 +0900

    Refactoring unify request methods

--- jewelpet/github/api.py
+++ jewelpet/github/api.py
@@ -7,30 +7,28 @@ from .types import Repository, User, PullRequest, Issue, IssueComment, Branch
 GITHUB_API = 'https://api.github.com'
 
 
-def _get(path):
+def _request(method, path, data=None):
     """
-    Request GET method
+    Request by any method
 
     Args:
+        <string> HTTP method
         <string> request path
     Returns:
         <dict|None> response JSON
     """
-    res = requests.get(
-        '%s%s' % (GITHUB_API, path),
+    return getattr(requests, method)(
+        '%s%s' % (GITHUB_API, path), json=data,
         headers={
             'Accept': 'application/vnd.github.v3+json',
             'Authorization': 'token %s' % settings['github']['token']
         },
         timeout=10)
-    if res.status_code != 200:
-        return None
-    return res.json()
 
 
-def _post(path, data):
+def _put(path, data):
     """
-    Request POST method
+    Request PUT method
 
     Args:
         <string> request path
@@ -38,57 +36,14 @@ def _post(path, data):
     Returns:
         <dict|None> response JSON
     """
-    res = requests.post(
+    res = requests.put(
         '%s%s' % (GITHUB_API, path), json=data,
         headers={
             'Accept': 'application/vnd.github.v3+json',
             'Authorization': 'token %s' % settings['github']['token']
         },
         timeout=10)
-    if res.status_code != 201:
-        return None
-    return res.json()
-
-
-def _patch(path, data):
-    """
-    Request PATCH method
-
-    Args:
-        <string> request path
-        <dict> request parameters
-    Returns:
-        <dict|None> response JSON
-    """
-    res = requests.patch(
-        '%s%s' % (GITHUB_API, path), json=data,
-        headers={
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': 'token %s' % settings['github']['token']
-        },
-        timeout=10)
-    if res.status_code != 200:
-        return None
-    return res.json()
-
-
-def _delete(path):
-    """
-    Request DELETE method
-
-    Args:
-        <string> request path
-    Returns:
-        <bool> success or not
-    """
-    res = requests.delete(
-        '%s%s' % (GITHUB_API, path),
-        headers={
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': 'token %s' % settings['github']['token']
-        },
-        timeout=10)
-    return res.status_code == 204
+    return res
 
 
 def _fill(params, keys):
@@ -112,15 +67,22 @@ def get_repo(owner, repo_name):
     Returns:
         <Repository|None>
     """
-    res = _get('/repos/%s/%s' % (owner, repo_name))
-    if res is None:
+    res = _request('get', '/repos/%s/%s' % (owner, repo_name))
+    if res.status_code != 200:
         return None
-    _fill(res, ('parent', 'source', 'organization'))
-    res['owner'] = _parse_user(res['owner'])
-    return Repository(**res)
+    params = res.json()
+    _fill(params, ('parent', 'source', 'organization'))
+    params['owner'] = _parse_user(params['owner'])
+    return Repository(**params)
 
 
 def _parse_user(params):
+    """
+    Args:
+        <dict>
+    Returns:
+        <User>
+    """
     for k in (
             'name',
             'company',
@@ -147,10 +109,10 @@ def get_user(name):
     Returns:
         <User|None>
     """
-    res = _get('/users/%s' % name)
-    if res is None:
+    res = _request('get', '/users/%s' % name)
+    if res.status_code != 200:
         return None
-    return User(**res)
+    return User(**res.json())
 
 
 def get_pr(owner, repo_name, pr_number):
@@ -162,16 +124,26 @@ def get_pr(owner, repo_name, pr_number):
     Returns:
         <PullRequest|None>
     """
-    res = _get('/repos/%s/%s/pulls/%d' % (owner, repo_name, pr_number))
-    if res is None:
+    res = _request('get', '/repos/%s/%s/pulls/%d' % (owner, repo_name, pr_number))
+    if res.status_code != 200:
         return None
-    res['links'] = res.pop('_links')  # namedtuple doesn't allow field name starts with a underscore
-    pr = PullRequest(**res)
-    pr_ = pr._replace(
-        user=_parse_user(pr.user),
-        assignee=_parse_user(pr.assignee),
-        assignees=[_parse_user(x) for x in pr.assignees])
-    return pr_
+    params = res.json()
+    params['links'] = params.pop('_links')  # namedtuple doesn't allow field name starts with a underscore
+    _parse_issue(params)
+    return PullRequest(**params)
+
+
+def _parse_issue(params):
+    """
+    Args:
+        <dict>
+    """
+    if params.get('user'):
+        params['user'] = _parse_user(params['user'])
+    if params.get('assignee'):
+        params['assignee'] = _parse_user(params['assignee'])
+    if params.get('assignees'):
+        params['assignees'] = [_parse_user(x) for x in params['assignees']]
 
 
 def get_issue(owner, repo_name, issue_number):
@@ -183,16 +155,13 @@ def get_issue(owner, repo_name, issue_number):
     Returns:
         <Issue|None>
     """
-    res = _get('/repos/%s/%s/issues/%d' % (owner, repo_name, issue_number))
-    if res is None:
+    res = _request('get', '/repos/%s/%s/issues/%d' % (owner, repo_name, issue_number))
+    if res.status_code != 200:
         return None
-    _fill(res, ('pull_request',))
-    issue = Issue(**res)
-    issue_ = issue._replace(
-        user=_parse_user(issue.user),
-        assignee=_parse_user(issue.assignee),
-        assignees=[_parse_user(x) for x in issue.assignees])
-    return issue_
+    params = res.json()
+    _fill(params, ('pull_request',))
+    _parse_issue(params)
+    return Issue(**params)
 
 
 def set_labels(owner, repo_name, issue_number, labels):
@@ -222,8 +191,8 @@ def assign(owner, repo_name, issue_number, assignees):
 
 
 def edit_issue(owner, repo_name, issue_number, **kwargs):
-    res = _patch('/repos/%s/%s/issues/%d' % (owner, repo_name, issue_number), kwargs)
-    return bool(res)
+    res = _request('patch', '/repos/%s/%s/issues/%d' % (owner, repo_name, issue_number), kwargs)
+    return res.status_code == 204
 
 
 def get_branch(owner, repo_name, branch_name):
@@ -235,25 +204,32 @@ def get_branch(owner, repo_name, branch_name):
     Returns:
         <Branch|None>
     """
-    res = _get('/repos/%s/%s/branches/%s' % (owner, repo_name, branch_name))
-    if res is None:
+    res = _request('get', '/repos/%s/%s/branches/%s' % (owner, repo_name, branch_name))
+    if res.status_code != 200:
         return None
-    res['links'] = res.pop('_links')  # namedtuple doesn't allow field name starts with a underscore
-    return Branch(**res)
+    params = res.json()
+    params['links'] = params.pop('_links')  # namedtuple doesn't allow field name starts with a underscore
+    return Branch(**params)
 
 
 def create_branch(owner, repo_name, branch_name, base_sha):
-    return _post('/repos/%s/%s/git/refs' % (owner, repo_name), {'ref': 'refs/heads/%s' % branch_name, 'sha': base_sha})
+    res = _request(
+        'post',
+        '/repos/%s/%s/git/refs' % (owner, repo_name),
+        {'ref': 'refs/heads/%s' % branch_name, 'sha': base_sha})
+    return res.status_code == 201
 
 
 def delete_branch(owner, repo_name, branch_name):
-    return _delete('/repos/%s/%s/git/refs/heads/%s' % (owner, repo_name, branch_name))
+    res = _request('delete', '/repos/%s/%s/git/refs/heads/%s' % (owner, repo_name, branch_name))
+    return res.status_code == 204
 
 
 def issue_comment(owner, repo_name, issue_number, comment):
-    res = _post(
+    res = _request(
+        'post',
         '/repos/%s/%s/issues/%d/comments' % (owner, repo_name, issue_number),
         {'body': comment})
-    if res is None:
+    if res.status_code != 201:
         return None
     return IssueComment(**res)
