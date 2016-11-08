8616a2f2a029624cd49631d1d45a7b5e871b0c69
Ryo Miyake <ryo.studiom@gmail.com>
2016-11-08 18:02:06 +0900

    rewnew impl around github

--- web.py
+++ web.py
@@ -32,46 +32,59 @@ def slack_app():
 @app.route('/github', methods=['POST'])
 def github_app():
     req = request.json
+    # from pprint import pprint
+    # pprint(req)
+
     if not github.is_valid_signature(
             request.data,
             request.headers.get('X-Hub-Signature')):
         return 'Invalid Signature'
-    try:
-        if req['action'] != 'created':
-            return 'PASS'
 
-        s = github.Session()
-        repo = s.find_repo(req['repository'])
+    if req['action'] != 'created':
+        return 'PASS'
+    (trigger, command, *args) = req['comment']['body'].split(' ')
 
-        (trigger, command, *args) = req['comment']['body'].split(' ')
-        if trigger in settings['github']['reviewers'] and command == 'r?':
-            issue = repo.get_issue(int(req['issue']['number']))
-            issue.edit(assignee=s.get_user(trigger[1:]))
-            issue.add_to_labels('S-awaiting-review')
-            return 'ASSIGNED'
+    print('trigger: %s' % trigger)
+    print('command: %s' % command)
+    print('args: %s' % args)
 
-        if trigger != settings['github']['trigger']:
-            return 'PASS'
+    if trigger in settings['github']['reviewers'] and command == 'r?':
+        owner = req['repository']['owner']['login']
+        repo_name = req['repository']['name']
+        issue_number = req['issue']['number']
+        github.edit_issue(owner, repo_name, issue_number, labels=['S-awaiting-review'], assignees=args)
+        return 'ASSIGNED'
 
-        if command in ('try', 'r+'):
-            pr_number = int(req['issue']['number'])
-            github.build_auto(repo, pr_number, command)
-            slack.post('created "auto" branch')
-            return 'AUTO'
-    except BranchConflictException:
-        slack.post('"auto" branch is already exists')
-        return 'AUTO ALREADY EXISTS'
-    except ValueError:
-        return 'ValueError'
-    except KeyError:
-        return 'KeyError'
+    if trigger != settings['github']['trigger']:
+        print(settings['github']['trigger'])
+        return 'PASS'
+    if command == 'r+':
+        sender = '@%s' % req['comment']['user']['login']
+        print('sender: %s' % sender)
+        if sender not in settings['github']['reviewers']:
+            return 'UNKNOWN_REVIEWER'
+        owner = req['repository']['owner']['login']
+        repo_name = req['repository']['name']
+        issue_number = req['issue']['number']
+        issue = github.get_issue(owner, repo_name, issue_number)
+        pr = github.get_pr(owner, repo_name, issue_number)
+
+        labels = [x['name'] for x in issue.labels if x['name'] != 'S-awaiting-review']
+        labels.append('S-awaiting-merge')
+        github.edit_issue(owner, repo_name, issue_number, labels=labels)
+        github.merge_pr(owner, repo_name, issue_number)
+
+        pr_head_label = pr.head['label'].split(':')
+        github.delete_branch(pr_head_label[0], repo_name, pr_head_label[1])
+        return 'MERGE'
+    return 'WHO'
 
 
 @app.route('/travis', methods=['POST'])
 def travis_app():
     req = json.loads(request.form.get('payload'))
-    from pprint import pprint
-    pprint(req)
+    # from pprint import pprint
+    # pprint(req)
     if req.get('branch', '') != 'auto':
         return 'PASS'
 
