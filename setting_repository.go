f8c0140fc213e6d31e221045d44a76e6c45fb883
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-12-13 16:25:22 +0900

    Move ReviewerSet to setting_repository.go

--- setting_repository.go
+++ setting_repository.go
@@ -19,3 +19,44 @@ func (r *repositoryInfo) isReviewer(name string) bool {
 func (r *repositoryInfo) Reviewers() *ReviewerSet {
 	return r.reviewers
 }
+
+type ReviewerSet struct {
+	regardAllAsReviewer bool
+	set                 map[string]*interface{}
+}
+
+func (s *ReviewerSet) Has(person string) bool {
+	if s.regardAllAsReviewer {
+		return true
+	}
+
+	_, ok := s.set[person]
+	return ok
+}
+
+func (s *ReviewerSet) Entries() []string {
+	list := make([]string, 0)
+	for k := range s.set {
+		list = append(list, k)
+	}
+	return list
+}
+
+func newReviewerSet(list []string, regardAllAsReviewer bool) *ReviewerSet {
+	if regardAllAsReviewer {
+		return &ReviewerSet{
+			true,
+			nil,
+		}
+	}
+
+	s := make(map[string]*interface{})
+	for _, name := range list {
+		s[name] = nil
+	}
+
+	return &ReviewerSet{
+		false,
+		s,
+	}
+}
