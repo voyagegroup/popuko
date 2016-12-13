c3122fc1afefa787a2501cce61164a993feb786d
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-12-13 17:21:02 +0900

    Add the test for RepositorySetting.ToRepoInfo()

---
+++ settings_config_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestRepositorySettingToRepoInfo1(t *testing.T) {
+	o := RepositorySetting{
+		shouldMergeAutomatically: true,
+		shouldDeleteMerged:       true,
+	}
+
+	ok, info := o.ToRepoInfo()
+	if !ok {
+		t.Fatal("should be success to convert from OwnersFile")
+	}
+
+	if !info.ShouldMergeAutomatically {
+		t.Fatal("ShouldMergeAutomatically: should be true")
+	}
+
+	if !info.ShouldDeleteMerged {
+		t.Fatal("ShouldDeleteMerged: should be true")
+	}
+}
