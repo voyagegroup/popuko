ae88183052110dc2df96b0ab622cf38c3de3cec6
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-12-14 15:52:44 +0900

    Implement to creating auto branch.
    - This is still experimental feature.
    - This patch is just a "part 1"
    - This introduce `repositoryInfo.ExperimentalTryOnAutoBranch()`
     - If it is true, this bot try to creat an auto branch when merge pull
       request.
     - This flag is only enabled if both of experimentalTryOnAutoBranch and
       EnableAutoMerge are true.

--- command_accept_changeset.go
+++ command_accept_changeset.go
@@ -75,6 +75,34 @@ func (c *AcceptCommand) commandAcceptChangesetByReviewer(ev *github.IssueComment
 	}
 
 	if c.info.EnableAutoMerge {
+		if c.info.ExperimentalTryOnAutoBranch() {
+			ok, _ := createBranchFromMaster(client.Git, repoOwner, repoName, "auto")
+			if !ok {
+				log.Println("info: cannot create the auto branch")
+				return false, nil
+			}
+			log.Println("info: create the auto branch")
+
+			ok, commit := mergeIntoAutoBranch(client.Repositories, repoOwner, repoName, pr.Head)
+			if !ok {
+				log.Println("info: cannot merge into the auto branch")
+				return false, nil
+			}
+
+			log.Println("info: merge the auto branch")
+
+			{
+				comment := ":hourglass: " + headSha + " has been merged into the auto branch " + *commit.HTMLURL
+				_, _, err := issueSvc.CreateComment(repoOwner, repoName, issue, &github.IssueComment{
+					Body: &comment,
+				})
+				if err != nil {
+					log.Println("info: could not create the comment to declare to merge this.")
+					return false, err
+				}
+			}
+		}
+
 		{
 			comment := ":hourglass: Try to merge " + headSha
 			_, _, err := issueSvc.CreateComment(repoOwner, repoName, issue, &github.IssueComment{
@@ -129,4 +157,52 @@ func (c *AcceptCommand) commandAcceptChangesetByOtherReviewer(ev *github.IssueCo
 	}
 
 	return c.commandAcceptChangesetByReviewer(ev)
+}
+
+func createBranchFromMaster(svc *github.GitService, owner string, repo string, branchName string) (ok bool, ref *github.Reference) {
+	refName := "refs/heads/" + branchName
+
+	log.Printf("info: clean up %v by deleting it\n", refName)
+	if _, err := svc.DeleteRef(owner, repo, refName); err != nil {
+		log.Printf("info: could not clean up %v by %v, but we continue to create %v optimistically\n", refName, err, refName)
+	}
+
+	const base string = "refs/heads/master"
+	ref, _, err := svc.GetRef(owner, repo, base)
+	if err != nil {
+		log.Printf("warn: cannot get reference about %v\n", base)
+		return
+	}
+
+	branchRef := github.Reference{
+		Ref:    &refName,
+		URL:    nil, // XXX: This field is unused on creating ref.
+		Object: ref.Object,
+	}
+
+	ref, _, err = svc.CreateRef(owner, repo, &branchRef)
+	if err != nil {
+		log.Printf("warn: cannot create a new ref %v\n", refName)
+		return
+	}
+
+	return true, ref
+}
+
+func mergeIntoAutoBranch(svc *github.RepositoriesService, owner string, repo string, head *github.PullRequestBranch) (ok bool, commit *github.RepositoryCommit) {
+	base := "auto"
+	message := "Auto merging"
+	req := github.RepositoryMergeRequest{
+		Base:          &base,
+		Head:          head.SHA,
+		CommitMessage: &message,
+	}
+
+	commit, _, err := svc.Merge(owner, repo, &req)
+	if err != nil {
+		log.Printf("warn: could not merge '%v' branch into master\n", base)
+		return
+	}
+
+	return true, commit
 }
