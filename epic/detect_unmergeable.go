932e0cd2316ff3ae182ddde8b090ba090e944c2a
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-01-07 03:36:54 +0900

    Clean up isMergeable()

--- epic/detect_unmergeable.go
+++ epic/detect_unmergeable.go
@@ -126,40 +126,31 @@ func markUnmergeable(wg *sync.WaitGroup, info *markUnmergeableInfo) {
 }
 
 func isMergeable(prSvc *github.PullRequestsService, owner string, name string, issue int) (bool, bool) {
-	ok, pr := getPrInfo(prSvc, owner, name, issue)
-	if !ok || pr == nil {
+	return innerIsMergeable(prSvc, owner, name, issue, 0)
+}
+
+func innerIsMergeable(prSvc *github.PullRequestsService, owner, name string, issue int, nest uint) (bool, bool) {
+	pr, _, err := prSvc.Get(owner, name, issue)
+	if err != nil || pr == nil {
+		log.Println("info: could not get the info for pull request")
+		log.Printf("debug: %v\n", err)
 		return false, false
 	}
 
 	mergeable := pr.Mergeable
 	if mergeable == nil {
 		// By the document https://developer.github.com/v3/pulls/#get-a-single-pull-request
-		// this state is still in checking.
+		// this state is still in checking if pr.Mergeable == nil.
+		if nest > 1 {
+			// We tried once.
+			// Conclude it is not mergeable heurístically
+			return true, false
+		}
 
 		// sleep same time: https://github.com/barosl/homu/blob/2104e4b154d2fba15d515b478a5bd6105c1522f6/homu/main.py#L722
 		time.Sleep(5 * time.Second)
-		ok, pr := getPrInfo(prSvc, owner, name, issue)
-		if !ok || pr == nil {
-			// Conclude it is not mergeable heurístically
-			return true, true
-		}
-
-		mergeable = pr.Mergeable
-		if mergeable == nil {
-			return true, false
-		}
+		return innerIsMergeable(prSvc, owner, name, issue, nest+1)
 	}
 
 	return true, *mergeable
-}
-
-func getPrInfo(prSvc *github.PullRequestsService, owner string, name string, issue int) (ok bool, info *github.PullRequest) {
-	pr, _, err := prSvc.Get(owner, name, issue)
-	if err != nil {
-		log.Println("info: could not get the info for pull request")
-		log.Printf("debug: %v\n", err)
-		return false, nil
-	}
-
-	return true, pr
 }
