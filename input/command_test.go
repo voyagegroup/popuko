47db0718fa5dc150b2403b8635ca09862e934180
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-01-13 00:57:35 +0900

    Parametalize parser test case for AcceptChangeByOthersCommand

--- input/command_test.go
+++ input/command_test.go
@@ -4,88 +4,6 @@ import (
 	"testing"
 )
 
-func TestParseCommand2(t *testing.T) {
-	ok, cmd := ParseCommand("@reviewer r?")
-	if !ok {
-		t.Errorf("should be ok")
-		return
-	}
-
-	v, ok := cmd.(*AssignReviewerCommand)
-	if !ok {
-		t.Errorf("should be AssignReviewerCommand")
-		return
-	}
-
-	if v.Reviewer != "reviewer" {
-		t.Errorf("should be the expected reviewer")
-		return
-	}
-}
-
-func TestParseCommand3(t *testing.T) {
-	ok, cmd := ParseCommand("@bot r=popuko,pipimi")
-	if !ok {
-		t.Errorf("should be ok")
-		return
-	}
-
-	v, ok := cmd.(*AcceptChangeByOthersCommand)
-	if !ok {
-		t.Errorf("should be AcceptChangeByOthersCommand")
-		return
-	}
-
-	if name := v.BotName(); name != "bot" {
-		t.Errorf("should be the expected bot name: %v\n", name)
-		return
-	}
-
-	if len(v.Reviewer) == 0 {
-		t.Errorf("should have some reviewers")
-		return
-	}
-
-	if name := v.Reviewer[0]; name != "popuko" {
-		t.Errorf("should be the expected reviewer 1: %v\n", name)
-		return
-	}
-
-	if name := v.Reviewer[1]; name != "pipimi" {
-		t.Errorf("should be the expected reviewer 2: %v\n", name)
-		return
-	}
-}
-
-func TestParseCommand21(t *testing.T) {
-	ok, cmd := ParseCommand("@bot r=popuko-a,pipimi-b")
-	if !ok {
-		t.Errorf("should be ok")
-		return
-	}
-
-	v, ok := cmd.(*AcceptChangeByOthersCommand)
-	if !ok {
-		t.Errorf("should be AcceptChangeByOthersCommand")
-		return
-	}
-
-	if name := v.BotName(); name != "bot" {
-		t.Errorf("should be the expected bot name: %v\n", name)
-		return
-	}
-
-	if name := v.Reviewer[0]; name != "popuko-a" {
-		t.Errorf("should be the expected reviewer 1: %v\n", name)
-		return
-	}
-
-	if name := v.Reviewer[1]; name != "pipimi-b" {
-		t.Errorf("should be the expected reviewer 2: %v\n", name)
-		return
-	}
-}
-
 func TestParseCommandValidCaseForAcceptChangeByReviewerCommand(t *testing.T) {
 	type TestCase struct {
 		input           string
@@ -140,6 +58,87 @@ func TestParseCommandValidCaseForAcceptChangeByReviewerCommand(t *testing.T) {
 		if actual := v.BotName(); actual != expected {
 			t.Errorf("input: `%v` should be the expected bot (`%v`) name but `%v`", input, expected, actual)
 			continue
+		}
+	}
+}
+
+func TestParseCommandValidCaseForAcceptChangeByOthersCommand(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected []string
+	}
+
+	list := []TestCase{
+		TestCase{
+			input:    "@bot r=popuko",
+			expected: []string{"popuko"},
+		},
+		TestCase{
+			input:    "  @bot    r=popuko  ",
+			expected: []string{"popuko"},
+		},
+
+		TestCase{
+			input:    "@bot r=popuko-a",
+			expected: []string{"popuko-a"},
+		},
+		TestCase{
+			input:    "  @bot    r=popuko-a ",
+			expected: []string{"popuko-a"},
+		},
+
+		TestCase{
+			input:    "@bot r=popuko,pipimi",
+			expected: []string{"popuko", "pipimi"},
+		},
+		TestCase{
+			input:    "  @bot r=popuko,pipimi   ",
+			expected: []string{"popuko", "pipimi"},
+		},
+		TestCase{
+			input:    "  @bot r=popuko,  pipimi   ",
+			expected: []string{"popuko", "pipimi"},
+		},
+
+		TestCase{
+			input:    "@bot r=popuko-a,pipimi-b",
+			expected: []string{"popuko-a", "pipimi-b"},
+		},
+		TestCase{
+			input:    "  @bot r=popuko-a,pipimi-b   ",
+			expected: []string{"popuko-a", "pipimi-b"},
+		},
+		TestCase{
+			input:    "  @bot r=popuko-a,   pipimi-b   ",
+			expected: []string{"popuko-a", "pipimi-b"},
+		},
+	}
+	for _, testcase := range list {
+		input := testcase.input
+
+		ok, cmd := ParseCommand(input)
+		if !ok {
+			t.Errorf("input: `%v` should be ok", input)
+			continue
+		}
+
+		v, ok := cmd.(*AcceptChangeByOthersCommand)
+		if !ok {
+			t.Errorf("input: `%v` should be AcceptChangeByOthersCommand", input)
+			continue
+		}
+
+		if len(v.Reviewer) != len(testcase.expected) {
+			t.Errorf("input: `%v` should be the expected length (`%v`) but the acutual length is `%v`", input, len(testcase.expected), len(v.Reviewer))
+			continue
+		}
+
+		for i, actual := range v.Reviewer {
+			expected := testcase.expected[i]
+			if actual != expected {
+				t.Errorf("input: `%v` should be the expected (`%v`) but `%v`", input, expected, actual)
+				continue
+			}
 		}
 	}
 }
