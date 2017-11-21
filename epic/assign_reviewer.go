a4b3b3e44731c1eb5c2a86d5196b5a3365ea33fc
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-11-21 11:06:18 +0900

    Don't assign a revirewer on an issue.
    Only do it on a pull request.

--- epic/assign_reviewer.go
+++ epic/assign_reviewer.go
@@ -18,24 +18,33 @@ func AssignReviewer(ctx context.Context, client *github.Client, ev *github.Issue
 	log.Printf("debug: repository owner is %v\n", repoOwner)
 	repo := *ev.Repo.Name
 	log.Printf("debug: repository name is %v\n", repo)
-	issue := *ev.Issue.Number
-	log.Printf("debug: issue number is %v\n", issue)
 
-	currentLabels := operation.GetLabelsByIssue(ctx, issueSvc, repoOwner, repo, issue)
+	issue := *ev.Issue
+	issueNum := *ev.Issue.Number
+	log.Printf("debug: issue number is %v\n", issueNum)
+
+	// https://godoc.org/github.com/google/go-github/github#Issue
+	// 	> If PullRequestLinks is nil, this is an issue, and if PullRequestLinks is not nil, this is a pull request.
+	if issue.PullRequestLinks == nil {
+		log.Println("info: the issue is pull request")
+		return false, nil
+	}
+
+	currentLabels := operation.GetLabelsByIssue(ctx, issueSvc, repoOwner, repo, issueNum)
 	if currentLabels == nil {
 		return false, nil
 	}
 
 	log.Printf("debug: assignees is %v\n", assignees)
 
-	_, _, err := issueSvc.AddAssignees(ctx, repoOwner, repo, issue, assignees)
+	_, _, err := issueSvc.AddAssignees(ctx, repoOwner, repo, issueNum, assignees)
 	if err != nil {
 		log.Println("info: could not change assignees.")
 		return false, err
 	}
 
 	labels := operation.AddAwaitingReviewLabel(currentLabels)
-	_, _, err = issueSvc.ReplaceLabelsForIssue(ctx, repoOwner, repo, issue, labels)
+	_, _, err = issueSvc.ReplaceLabelsForIssue(ctx, repoOwner, repo, issueNum, labels)
 	if err != nil {
 		log.Println("info: could not change labels.")
 		return false, err
